fc4f7434256548e2e414427d43ba51bba117e8be
ishika749 <ishikashah749@gmail.com>
2025-05-14 07:56:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+function mapLetters(word) {
+  const result = {};
+  
+  // Iterate through the word with index
+  word.split('').forEach((letter, index) => {
+    // Create symbol for the letter
+    const symbol = Symbol(letter);
+    
+    // If symbol doesn't exist in result, initialize array
+    if (!result[symbol]) {
+      result[symbol] = [];
+    }
+    
+    // Push current index to the array
+    result[symbol].push(index);
+  });
+  
+  return result;
+}
